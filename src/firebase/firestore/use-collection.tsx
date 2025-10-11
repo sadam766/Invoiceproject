023dce449b0f89a72f07f5edd829276b85a32dec
sadam gdemahendrawww <sadamgdemahendrawww@gmail.com>
2025-10-11 14:16:52 +0000

    Try fixing this error: `Runtime Error: Error: Firebase core services not

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -108,7 +108,7 @@ export function useCollection<T = any>(
     return () => unsubscribe();
   }, [memoizedTargetRefOrQuery]); // Re-run if the target query/reference changes.
   if(memoizedTargetRefOrQuery && !memoizedTargetRefOrQuery.__memo) {
-    throw new Error(memoizedTargetRefOrQuery + ' was not properly memoized using useMemoFirebase');
+    throw new Error('useCollection query was not properly memoized using useMemoFirebase. This will cause an infinite loop.');
   }
   return { data, isLoading, error };
 }
--- src/firebase/firestore/use-doc.tsx
+++ src/firebase/firestore/use-doc.tsx
@@ -1,7 +1,6 @@
-
 'use client';
     
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import {
   DocumentReference,
   onSnapshot,
@@ -40,7 +39,7 @@ export interface UseDocResult<T> {
  * @returns {UseDocResult<T>} Object with data, isLoading, error.
  */
 export function useDoc<T = any>(
-  memoizedDocRef: DocumentReference<DocumentData> | null | undefined,
+  memoizedDocRef: (DocumentReference<DocumentData> & {__memo?: boolean}) | null | undefined,
 ): UseDocResult<T> {
   type StateDataType = WithId<T> | null;
 
@@ -89,6 +88,10 @@ export function useDoc<T = any>(
 
     return () => unsubscribe();
   }, [memoizedDocRef]); // Re-run if the memoizedDocRef changes.
+
+  if(memoizedDocRef && !memoizedDocRef.__memo) {
+    throw new Error('useDoc reference was not properly memoized using useMemoFirebase. This will cause an infinite loop.');
+  }
 
   return { data, isLoading, error };
 }
