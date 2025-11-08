bb321451796c9409e3f2c86ea233bcdea284cff4
sadam gdemahendrawww <sadamgdemahendrawww@gmail.com>
2025-11-08 09:40:28 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/app/dashboard/invoices/add/page.tsx
+++ src/app/dashboard/invoices/add/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import {
   Card,
