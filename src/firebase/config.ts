d9e38e99ff358315ebaa1bb2fb77d67689754ed0
sadam gdemahendrawww <sadamgdemahendrawww@gmail.com>
2026-04-26 02:38:29 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyA641pV1OC68S3SBYzJ-mpT_xcBlVTcV4c",
-  authDomain: "studio-7965178539-236f3.firebaseapp.com",
-  projectId: "studio-7965178539-236f3",
-  storageBucket: "studio-7965178539-236f3.firebasestorage.app",
-  messagingSenderId: "432612300615",
-  appId: "1:432612300615:web:f406b4ee6ea643b80e5e62"
+  "projectId": "studio-7965178539-236f3",
+  "appId": "1:432612300615:web:f406b4ee6ea643b80e5e62",
+  "apiKey": "AIzaSyA641pV1OC68S3SBYzJ-mpT_xcBlVTcV4c",
+  "authDomain": "studio-7965178539-236f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "432612300615"
 };
